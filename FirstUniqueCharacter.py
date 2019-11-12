ea922ec10b2d05115eae288514b2347824267c46
c0n4tus <shivamjakhanwal@gmail.com>
2019-11-11 18:57:01 -0800

    Prob 41: First Unique Character leet

---
+++ FirstUniqueCharacter.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dic={}
+        ky=None
+        
+        for i in range(0,len(s)):
+            if not dic.get(s[i]):
+                dic[s[i]]=1
+            else:
+                dic[s[i]]=dic.get(s[i])+1
+                
+        for k in dic:
+            if dic.get(k) == 1:
+                ky=k
+                break
+        if ky:
+            return s.index(ky)
+        else:
+            return -1
